41093e39066189b45000bedc122b10489150a9c4
kamaukimani <paulkimys285@gmail.com>
2026-09-05 20:29:04 +0300

    seed emloyees table also

--- app/seed.py
+++ app/seed.py
@@ -1,11 +1,17 @@
 from models.department import Department
+from models.employee import Employee
 from models.__init__ import CURSOR
 from faker import Faker 
+import random
 
 
-# fake=Faker()
+fake=Faker()
+# Employee.drop_table()
 # Department.drop_table()
 # Department.create_table()
+# Employee.create_table()
+
+
 
 # departments=[]
 # for i in range(20):
@@ -14,8 +20,25 @@ from faker import Faker
 #         location=fake.unique.address()
 #     )
 #     departments.append(department)
+# employees=[]
+# for department in departments:
+#     for i in range(random.randint(1,3)):
+#         employee=Employee.create(
+#             name=fake.unique.name(),
+#             job_title=fake.word(),
+#             department_id=department.id
+#         )
+#         employees.append(employee)
 
-print(Department.get_all())
-print(".................")
-print(Department.find_by_id(2))
-print(Department.find_by_name("Dakota Bell"))
+
+# print(Department.get_all())
+# print(".................")
+# print(Department.find_by_id(2))
+# print(Department.find_by_name("Dakota Bell"))
+# sql="SELECT * FROM departments WHERE id=?;"
+# dep1=CURSOR.execute(sql,(1,)).fetchone()
+# print(dep1)
+# print(dep1.employees())
+dep1=Department.find_by_id(1)
+print(dep1)
+print(dep1.employees())
